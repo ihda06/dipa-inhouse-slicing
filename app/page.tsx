eb1a012c59e22f54eca1cc44be8d94993d48948a
ihda <ihdaanwari5@gmail.com>
2025-12-14 12:47:19 +0700

    feat: restructure landing page components and add new sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/header/Header";
-import HeroSection from "@/components/hero/HeroSection";
+import Header from "@/components/landing-page/header/Header";
+import HeroSection from "@/components/landing-page/hero/HeroSection";
 import { ToolkitSection } from "@/components/toolkit/ToolkitSection";
-import EnterpriseSection from "@/components/enterprise/EnterpriseSection";
-import PlatformSection from "@/components/platform/PlatformSection";
-import CTASection from "@/components/cta/CTASection";
-import { PricingSection } from "@/components/pricing/PricingSection";
-import Footer from "@/components/footer/Footer";
+import EnterpriseSection from "@/components/landing-page/enterprise/EnterpriseSection";
+import PlatformSection from "@/components/landing-page/platform/PlatformSection";
+import CTASection from "@/components/landing-page/cta/CTASection";
+import { PricingSection } from "@/components/landing-page/pricing/PricingSection";
+import Footer from "@/components/landing-page/footer/Footer";
 
 export default function Home() {
   return (
